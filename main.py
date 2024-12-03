c69217fb2c35ff6368eb91dc5af661d1f9b986d2
Mael <astrion200@gmail.com>
2024-12-03 14:45:45 +0100

    add some basics functions

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from final_challenge import *
 from math_challenges import *
 from logical_challenges import *
 from pere_fouras_challenge import *
+from utility_functions import *
 
 print(nearest_prime(17))
+print(ChoosePlayer("mael", "jalil", "hey"))
