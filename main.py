44a15165b9b7eb2ee11c28215c1fd2fae942a30a
Mael <astrion200@gmail.com>
2024-12-06 12:28:07 +0100

    challenge function available

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cgitb import reset
+
 from chance_challenges import *
 from final_challenge import *
 from math_challenges import *
@@ -5,6 +7,10 @@ from logical_challenges import *
 from pere_fouras_challenge import *
 from utility_functions import *
 
-Reset()
-math_challenge_factorial()
+reset()
 
+challenge = math_challenge()
+challenge()
+
+challenge = chance_challenge()
+challenge()
