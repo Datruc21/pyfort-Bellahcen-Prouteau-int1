bc65830f999c8312eb4c7a9e058416d272c38c65
Mael <astrion200@gmail.com>
2024-12-06 10:33:38 +0100

    test

--- logical_challenges.py
+++ logical_challenges.py
@@ -1,14 +1,4 @@
 
-def next_player(n) :
-    if n == 1 :
-        return 0
-    return 1
-
-def empty_grid() :
-    grid = []
-    for i in range(3) :
-        l = []
-        for j in range(3) :
-            l.append(" ")
-        grid.append(l)
-    return grid
+def test():
+    a=5
+    return a
