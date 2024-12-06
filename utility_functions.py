321359a940e5ef4ee25e3e0f5f36486971a821ea
Mael <astrion200@gmail.com>
2024-12-06 13:09:37 +0100

    challenge function available

--- utility_functions.py
+++ utility_functions.py
@@ -15,19 +15,12 @@ def compose_team():
         player_number = int(input("You cannot have more than 3 players in your team : "))
     player = [{}, {}, {}]
     for i in range(player_number) :
-        player[i][1] = input("Enter the name of this player :")
-        player[i][2] = input("Enter its profession :")
-        player[i][3] = input("Is it the leader ? Enter yes if this person is.") == 'yes'
+        player[i]["name"] = input("Enter the name of this player :")
+        player[i]["profession"] = input("Enter its profession :")
+        player[i]["leader"] = input("Is it the leader ? Enter yes if this person is.") == 'yes'
+        player[i]["keys_won"] = 0
+
+    for i in range(player_number) : # We check whether there is one leader or not, we will keep the first one if there are many or designate the first player if there is noone.
 
     print(player)
 
-
-def ChoosePlayer(p1, p2 = "", p3 = "") :
-
-    list_player = [p1]
-    if p2 != "" :
-        list_player.append(p2)
-        if p3!= "" : list_player.append(p3)
-
-    player = randint(0,len(list_player)-1)
-    return list_player[player]
