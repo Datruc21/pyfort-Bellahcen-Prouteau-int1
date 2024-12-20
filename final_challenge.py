6773ebb6879b51ee52b06916c1919cf0a29979ba
Mael <astrion200@gmail.com>
2024-12-20 10:38:39 +0100

    final challenge working

--- final_challenge.py
+++ final_challenge.py
@@ -1,13 +1,14 @@
 from random import *
 from utility_functions import*
 import json
-def treasure_room() :
+def treasure_room() :                   #The main function simulating the trial
     with open("Data\TRClues.json", "r", encoding="utf-8") as f:
         tv_game = json.load(f)
         year = str(randint(2015,2019))
         D = tv_game["Fort Boyard"][year]
         show = D[choice(list(D.keys()))]
-        print("Clues:\n-{}\n-{}\n-{}".format(show["Clues"][0],show["Clues"][1],show["Clues"][2]))
+        remaining_clues = show["Clues"]
+        print("Clues:\n-{}\n-{}\n-{}".format(remaining_clues.pop(randint(0,len(remaining_clues)-1)),remaining_clues.pop(randint(0,len(remaining_clues)-1)),remaining_clues.pop(randint(0,len(remaining_clues)-1))))
         correct_answer = show["CODE-WORD"]
         tries = 3
         while tries>0 :
@@ -23,6 +24,6 @@ def treasure_room() :
                 return True
             elif tries > 1 :
                 tries -= 1
-                print("Missed, number of tries left : {}, here is another hint : {}".format(tries, show["Clues"][-(tries+1)]))
+                print("Missed, number of tries left : {}, here is another hint : {}".format(tries, remaining_clues.pop(randint(0,len(remaining_clues)-1))))  #show["Clues"][-(tries+1)]
             else : tries -= 1
-        print("What a pity, the last hint was : {} and the answer was {}".format(show["Clues"][5],correct_answer))
+        print("What a pity, the answer was {}".format(correct_answer))
