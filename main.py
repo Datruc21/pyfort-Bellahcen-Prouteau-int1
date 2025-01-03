f9b61a21223e2275fc51e168380146199c05e64e
Mael <astrion200@gmail.com>
2025-01-03 14:59:59 +0100

    Last verifications

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 """         PYFORT by Maël and Jalil
-The main file (done by Maël), used to run the game"""
+The main file (done by Maël), is used to run the game"""
 
 from chance_challenges import *
 from final_challenge import *
-from math_challenges import *
-from logical_challenges import *
-from pere_fouras_challenge import *
 from utility_functions import *
 battleship_game()
 
